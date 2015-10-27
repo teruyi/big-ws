de5126338f87c1479c8b7a50df3cf4a605c1c372
Sergio Soro <ssm.fuentes@gmail.com>
2015-10-27 11:58:06 +0100

    practica2 travis.yml

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,8 +20,8 @@ public class TranslatorServiceTest {
     TranslatorService translatorService;
     @Test
     public void translateTest() throws Exception {
-        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-        assertEquals("Esto es una prueba de servicio de traducción",translatedText.getTranslation());
+        TranslatedText translatedText = translatorService.translate("en", "es", "i'm Sergio, who are you?");
+        assertEquals("yo soy sergio, ¿quién eres tú?",translatedText.getTranslation());
     }
 
 }
